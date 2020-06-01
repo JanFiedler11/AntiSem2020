ca44f26ce8e00ee5526074163f981288fd22e5fc
JanFiedler <jan.fiedler.b@gmail.com>
2020-06-02 00:09:26 +0200

    added function for cleaning data, aplling label filter and upsampling minority class

--- main.py
+++ main.py
@@ -1,9 +1,37 @@
 import json
 import pandas as pd
 import numpy as np
+import re
+from sklearn.utils import resample
 
 gold_data='hackathon.json'
 
+'''
+clean the text data from @users, emojis and other stuff
+'''
+def clean_data(df,field):
+    df[field]=df[field].str.lower()
+    df[field] = df[field].apply(lambda elem: re.sub(r"(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)|^rt|http.+?", "", elem))
+    return df
+
+'''
+apply a binary filter for the gold data.
+antisemtic level: 1-3 = not antisemitic
+antisemtic level: 4-5 = antisemitic
+'''
+def apply_binary_label(df,field):
+    k=0
+    for i in df[field]:
+        if int(i)>3:
+            df[field][k]="1"
+        else:
+            df[field][k]="0"
+        k=k+1
+    return df
+
+'''
+split the gold standard from json into test and training in a panda dataframe
+'''
 def split_test_training_from_json(file):
     with open (file,'r') as read_file:
         data=json.load(read_file)
@@ -37,7 +65,32 @@ def split_test_training_from_json(file):
 
 
 if __name__ == "__main__":
+    #getting the tweets with labeled in data frame for test and training
     training_data,test_data=split_test_training_from_json(gold_data)
-    print(training_data)
-    print(test_data)    
 
+    #generate debug files
+    #training_data.to_csv('csv/training_data.csv')
+    #test_data.to_csv('csv/test_data.csv')
+
+    #clean
+    clean_training=clean_data(training_data,"Text")
+    clean_test=clean_data(test_data,"Text")
+
+    clean_training=apply_binary_label(clean_training,"Label")
+    clean_test=apply_binary_label(clean_test,"Label")
+
+    #generate debug files
+    clean_training.to_csv('csv/clean_training.csv')
+    clean_test.to_csv('csv/clean_test.csv')
+
+    #they are like 100 more antisemtic tweets than not antisemitic. Thats why we are upsampling the minority.
+    train_majority=clean_training[clean_training.Label=="0"]
+    train_minority=clean_training[clean_training.Label=="1"]
+    
+    train_minority_upsampled=resample(train_minority,replace=True,n_samples=len(train_majority),random_state=123)
+    train_upsampled=pd.concat([train_minority_upsampled,train_majority])
+
+    print(train_upsampled['Label'].value_counts())
+    
+    #exit(0)
+    print("done")
